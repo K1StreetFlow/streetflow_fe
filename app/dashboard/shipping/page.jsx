8021a907a0b08b221aa0e4dc6ad1de55682d4b80
rizkirifandi7 <rizki.10121284@mahasiswa.ac.id>
2023-11-20 23:22:20 +0700

    update admin orderlist

--- app/dashboard/shipping/page.jsx
+++ app/dashboard/shipping/page.jsx
@@ -1,6 +1,5 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import TableOrder from "@/app/dashboard/shipping/TableOrder";
-import TableOrderBeta from "@/app/dashboard/shipping/TableOrderBeta";
 
 const OrderList = () => {
 	return (
@@ -8,7 +7,6 @@ const OrderList = () => {
 			<Breadcrumb pageName="Order List" />
 			<div className="rounded-sm border border-stroke bg-white p-4 shadow-default dark:border-strokedark dark:bg-boxdark md:p-6 xl:p-9">
 				<TableOrder />
-				<TableOrderBeta />
 			</div>
 		</>
 	);
