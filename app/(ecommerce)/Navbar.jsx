64b484c502741cadeddca99c52d0f3ef47f20174
rizkirifandi7 <rizki.10121284@mahasiswa.ac.id>
2023-11-24 19:34:31 +0700

    update order list

--- app/(ecommerce)/Navbar.jsx
+++ app/(ecommerce)/Navbar.jsx
@@ -85,87 +85,73 @@ export default function Navbar() {
 		return () => clearInterval(interval);
 	}, []);
 
-  return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li className="text-black font-bold">
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/product">Store</a>
-            </li>
-          </ul>
-        </div>
-        <a className="cursor-pointer p-5">
-          <Image
-            src="/images/logo/streetflow-logo.svg"
-            width={200}
-            height={200}
-            className="w-40 rounded "
-            alt="Logo Streetflow"
-          />
-        </a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/product">Store</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end me-5">
-        <div className="flex flex-row me-5">
-          {showCartIcon && (
-            <>
-              <Link href="/carts">
-                <Image
-                  src="/images/icon/shop-cart-bold.svg"
-                  width={30}
-                  height={30}
-                />
-              </Link>
-              {cart.total_product > 0 && (
-                <span className="badge bg-[#3C50E0] py-2 text-white font-bold ">
-                  {cart.total_product}
-                </span>
-              )}
-            </>
-          )}
-        </div>
-        {userData ? (
-          <DropdownUser user={userData} />
-        ) : (
-          // <a className="btn">Logout</a>
+	return (
+		<div className="navbar bg-base-100">
+			<div className="navbar-start">
+				<div className="dropdown">
+					<label tabIndex={0} className="btn btn-ghost lg:hidden">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+						</svg>
+					</label>
+					<ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+						<li className="text-black font-bold">
+							<a href="/">Home</a>
+						</li>
+						<li>
+							<a href="/about">About</a>
+						</li>
+						<li>
+							<a href="/product">Store</a>
+						</li>
+					</ul>
+				</div>
+				<a className="cursor-pointer p-5">
+					<Image
+						src="/images/logo/streetflow-logo.svg"
+						width={200}
+						height={200}
+						className="w-40 rounded "
+						alt="Logo Streetflow"
+					/>
+				</a>
+			</div>
+			<div className="navbar-center hidden lg:flex">
+				<ul className="menu menu-horizontal px-1">
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a href="/product">Store</a>
+					</li>
+				</ul>
+			</div>
+			<div className="navbar-end me-5">
+				<div className="flex flex-row me-5">
+					{showCartIcon && (
+						<>
+							<Link href="/carts">
+								<Image src="/images/icon/shop-cart-bold.svg" width={30} height={30} />
+							</Link>
+							{cart.total_product > 0 && (
+								<span className="badge bg-[#3C50E0] py-2 text-white font-bold ">{cart.total_product}</span>
+							)}
+						</>
+					)}
+				</div>
+				{userData ? (
+					<DropdownUser user={userData} />
+				) : (
+					// <a className="btn">Logout</a>
 
 					<a className="btn">Login</a>
 				)}
