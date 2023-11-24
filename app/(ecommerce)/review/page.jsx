279f430dbefc725575c8ba05ad485c6ab1372bc5
ReCodink <muhammadraihan1607@gmail.com>
2023-11-24 23:10:23 +0700

    Update Review

---
+++ app/(ecommerce)/review/page.jsx
@@ -0,0 +1,21 @@
+// import SidebarCustomer from "@/components/Sidebar/SidebarCustomer";
+// import ReviewPages from "./Reviews";
+// const Reviews = () => {
+// 	return (
+// 		<>
+// 			<div className="flex items-start mt-10">
+// 				<div className="w-1/5 border-none rounded-lg pb-8 box text-[#212121]">
+// 					<SidebarCustomer />
+// 				</div>
+// 				<div className="w-4/5 mb-5 text-[#212121]">
+// 					<h3 className="font-bold ml-6 mb-5 text-xl">Reviews</h3>
+// 					<div className="box-2">
+// 						<ReviewPages />
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</>
+// 	);
+// };
+
+// export default Reviews;
