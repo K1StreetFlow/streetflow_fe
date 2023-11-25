43eb713ffce5bae908d2950e009997f06ec4e11d
alvianori <alvianorck@gmail.com>
2023-11-26 03:48:17 +0700

    fix product, review product, dan payment

--- app/dashboard/payments/[id]/page.jsx
+++ app/dashboard/payments/[id]/page.jsx
@@ -1,158 +1,187 @@
+// import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+// import { formatDate } from "@/app/utils/formatDate";
+
+// async function getPaymentById(id) {
+//   const res = await fetch(`http://localhost:8000/api/payments/${id}`, {
+//     credentials: "include",
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+//   return res.json();
+// }
+
+// const Payment = async ({ params }) => {
+//   const data = await getPaymentById(params.id);
+
+//   console.log("params.id:", params.id);
+//   console.log("API Response:", data);
+
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { formatDate } from "@/app/utils/formatDate";
 
-async function getPaymentById(id) {
-	const res = await fetch(`http://localhost:8000/api/payments/${id}`, {
-		withCredentials: true,
-		next: {
-			revalidate: 0,
-		},
-	});
-	return res.json();
-}
+const Payment = ({ params }) => {
+  const [data, setData] = useState(null);
 
-const Payment = async ({ params }) => {
-	const { data } = await getPaymentById(params.id);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/payments/${params.id}`, {
+          withCredentials: true,
+        });
+        setData(response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-	console.log(data);
+    fetchData();
+  }, [params.id]); // Menerapkan useEffect setiap kali params.id berubah
 
-	return (
-		<>
-			<Breadcrumb pageName="Payment" />
+  console.log("params.id:", params.id);
+  console.log("API Response:", data);
 
-			<div className="rounded-sm border border-stroke bg-white p-4 shadow-default dark:border-strokedark dark:bg-boxdark md:p-6 xl:p-9 ">
-				<h2 className="mb-5 font-bold text-xl">Customer Detail</h2>
-				<div className="flex flex-col gap-7.5">
-					<div className="flex flex-row gap-5.5 ">
-						<div className="w-full sm:w-1/2">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Full Name
-							</label>
-							<div className="relative">
-								<h3>{data.cart.user_customer.fullname}</h3>
-							</div>
-						</div>
+  if (!data) {
+    // Menampilkan indikator loading atau pesan bahwa data sedang dimuat
+    return <p>Loading...</p>;
+  }
 
-						<div className="w-full sm:w-1/2">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="email">
-								Username
-							</label>
+  return (
+    <>
+      <Breadcrumb pageName="Payment" />
 
-							<h3>{data.cart.user_customer.username}</h3>
-						</div>
-						<div className="w-full sm:w-1/2">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="phoneNumber">
-								Phone Number
-							</label>
+      <div className="rounded-sm border border-stroke bg-white p-4 shadow-default dark:border-strokedark dark:bg-boxdark md:p-6 xl:p-9 ">
+        <h2 className="mb-5 font-bold text-xl">Customer Detail</h2>
+        <div className="flex flex-col gap-7.5">
+          <div className="flex flex-row gap-5.5 ">
+            <div className="w-full sm:w-1/2">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Full Name
+              </label>
+              <div className="relative">
+                <h3>{data?.cart?.user_customer?.fullname || "N/A"}</h3>
+              </div>
+            </div>
 
-							<h3>{data.cart.user_customer.phone_number}</h3>
-						</div>
-					</div>
-					<div className="flex flex-col gap-5.5 sm:flex-row">
-						<div className="w-[32%]">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Gender
-							</label>
-							<div className="relative">
-								<h3>{data.cart.user_customer.gender}</h3>
-							</div>
-						</div>
-						<div className="w-110">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Email
-							</label>
-							<div className="relative">
-								<h3>{data.cart.user_customer.email}</h3>
-							</div>
-						</div>
-					</div>
-				</div>
-				<hr className="mt-10" />
-				<h2 className="mb-5 mt-10 font-bold text-xl">Payment Detail</h2>
-				<div className="flex flex-col gap-7.5 mb-20">
-					<div className="flex flex-row gap-5.5 ">
-						<div className="w-full sm:w-1/2">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Payment Method
-							</label>
-							<div className="relative">
-								<h3>{data.method_payment}</h3>
-							</div>
-						</div>
+            <div className="w-full sm:w-1/2">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="email">
+                Username
+              </label>
 
-						<div className="w-full sm:w-1/2">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="email">
-								Total Payment
-							</label>
+              <h3>{data?.cart?.user_customer?.username || "N/A"}</h3>
+            </div>
+            <div className="w-full sm:w-1/2">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="phoneNumber">
+                Phone Number
+              </label>
 
-							<h3>Rp {data.total_payment}</h3>
-						</div>
-						<div className="w-full sm:w-1/2">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="email">
-								Total Payment
-							</label>
+              <h3>{data?.cart?.user_customer?.phone_number || "N/A"}</h3>
+            </div>
+          </div>
+          <div className="flex flex-col gap-5.5 sm:flex-row">
+            <div className="w-[32%]">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Gender
+              </label>
+              <div className="relative">
+                <h3>{data?.cart?.user_customer?.gender || "N/A"}</h3>
+              </div>
+            </div>
+            <div className="w-110">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Email
+              </label>
+              <div className="relative">
+                <h3>{data?.cart?.user_customer?.email || "N/A"}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="mt-10" />
+        <h2 className="mb-5 mt-10 font-bold text-xl">Payment Detail</h2>
+        <div className="flex flex-col gap-7.5 mb-20">
+          <div className="flex flex-row gap-5.5 ">
+            <div className="w-full sm:w-1/2">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Payment Method
+              </label>
+              <div className="relative">
+                <h3>{data?.method_payment || "N/A"}</h3>
+              </div>
+            </div>
 
-							<h3>Rp {data.total_payment}</h3>
-						</div>
-						<div className="w-full sm:w-1/2">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="phoneNumber">
-								Status Payment
-							</label>
+            <div className="w-full sm:w-1/2">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="email">
+                Total Payment
+              </label>
 
-							<p
-								className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${
-									data.status_payment === "Success"
-										? "text-success bg-success"
-										: data.status_payment === "Failed"
-										? "text-danger bg-danger"
-										: "text-warning bg-warning"
-								}`}
-							>
-								{data.status_payment}
-							</p>
-						</div>
-					</div>
-					<div className="flex flex-col gap-5.5 sm:flex-row mt-5">
-						<div className="w-[32%]">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Date Payment
-							</label>
-							<div className="relative">
-								<h3>{formatDate(data.date_payment)}</h3>
-							</div>
-						</div>
-						<div className="w-1/3">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Code Payment
-							</label>
-							<div className="relative">
-								<h3>{data.code_payment}</h3>
-							</div>
-						</div>
-						<div className="w-1/3">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Virtual Account Type
-							</label>
-							<div className="relative">
-								<h3>{data.va_type}</h3>
-							</div>
-						</div>
-						<div className="w-1/3">
-							<label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-								Virtual Account
-							</label>
-							<div className="relative">
-								<h3>{data.va_number}</h3>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div>
-					<button></button>
-				</div>
-			</div>
-		</>
-	);
+              <h3>Rp {data?.total_payment || "N/A"}</h3>
+            </div>
+            <div className="w-full sm:w-1/2">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="email">
+                Total Payment
+              </label>
+
+              <h3>Rp {data?.total_payment || "N/A"}</h3>
+            </div>
+            <div className="w-full sm:w-1/2">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="phoneNumber">
+                Status Payment
+              </label>
+
+              <p
+                className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${
+                  data?.status_payment === "Success" ? "text-success bg-success" : data?.status_payment === "Failed" ? "text-danger bg-danger" : "text-warning bg-warning"
+                }`}
+              >
+                {data?.status_payment || "N/A"}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-5.5 sm:flex-row mt-5">
+            <div className="w-[32%]">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Date Payment
+              </label>
+              <div className="relative">
+                <h3>{formatDate(data?.date_payment || "N/A")}</h3>
+              </div>
+            </div>
+            <div className="w-1/3">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Code Payment
+              </label>
+              <div className="relative">
+                <h3>{data?.code_payment || "N/A"}</h3>
+              </div>
+            </div>
+            <div className="w-1/3">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Virtual Account Type
+              </label>
+              <div className="relative">
+                <h3>{data?.va_type || "N/A"}</h3>
+              </div>
+            </div>
+            <div className="w-1/3">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                Virtual Account
+              </label>
+              <div className="relative">
+                <h3>{data?.va_number || "N/A"}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button></button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Payment;
--- app/dashboard/products/reviews/TableReviewProducts.jsx
+++ app/dashboard/products/reviews/TableReviewProducts.jsx
@@ -1,124 +1,150 @@
+"use client";
 import Image from "next/image";
 // import AddReview from "@/app/dashboard/products/reviews/addReviewProducts";
 import EditReview from "@/app/dashboard/products/reviews/editReviewProducts";
 import DeleteReview from "@/app/dashboard/products/reviews/deleteReviewProducts";
 import DetailReview from "@/app/dashboard/products/reviews/detailReviewProducts";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-async function getAllReviewProducts() {
-    const res = await fetch("http://localhost:8000/api/review-products", {
-        next: {
-            revalidate: 0,
-        },
-    });
-    return res.json();
-}
+// async function getAllReviewProducts() {
+//   const res = await fetch("http://localhost:8000/api/review-products", {
+//     credentials: "include",
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+//   return res.json();
+// }
 
-async function getAllCustomer() {
-    const res = await fetch("http://localhost:8000/api/user", {
-        next: {
-            revalidate: 0,
-        },
-    });
-    return res.json();
-}
+// async function getAllCustomer() {
+//   const res = await fetch("http://localhost:8000/api/user", {
+//     credentials: "include",
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+//   return res.json();
+// }
 
-async function getAllProducts() {
-    const res = await fetch("http://localhost:8000/api/products", {
-        next: {
-            revalidate: 0,
-        },
-    });
-    return res.json();
-}
+// async function getAllProducts() {
+//   const res = await fetch("http://localhost:8000/api/products", {
+//     credentials: "include",
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+//   return res.json();
+// }
 
+// async function getAllOrderList() {
+//   const res = await fetch("http://localhost:8000/api/order", {
+//     credentials: "include",
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+//   return res.json();
+// }
 
-async function getAllOrderList() {
-    const res = await fetch("http://localhost:8000/api/order", {
-        next: {
-            revalidate: 0,
-        },
-    });
-    return res.json();
-}
+// const TableReviewProducts = async () => {
+//   let count = 1;
 
+//   const reviews = await getAllReviewProducts();
+//   const users = await getAllCustomer();
+//   const products = await getAllProducts();
+//   const order_list = await getAllOrderList();
 
+//console.log(reviews);
 
-const TableReviewProducts = async () => {
-    let count = 1;
-    
-    const reviews = await getAllReviewProducts();
-    const users = await getAllCustomer();
-    const products = await getAllProducts();
-    const order_list = await getAllOrderList();
-    
-    console.log(reviews);
-    return (
-        <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-            <div className="py-6 px-4 md:px-6 xl:px-7.5">
-                {/* <AddReview users={users} products={products} order_list={order_list} /> */}
-            </div>
+const TableReviewProducts = () => {
+  const [reviews, setReviews] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [orderList, setOrderList] = useState([]);
 
-            <div className="py-10 px-10">
-                <table className="table w-full">
-                    <thead>
-                        <tr className="bg-gray-2 text-left dark:bg-meta-4">
-                            <th className="py-4 px-4 font-medium text-black dark:text-white">#</th>
-                            <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                                Customer Name
-                            </th>
-                            <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                                Product Name
-                            </th>
-                            <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                                Code Order
-                            </th>
-                            <th className="py-4 px-4 font-medium text-black dark:text-white">
-                                Actions
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {reviews.data?.map((review, key) => (
-                            <tr key={key}>
-                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                    <h6 className="font-medium text-black dark:text-white">
-                                        {review.id}
-                                    </h6>
-                                </td>
-                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                    <h5 className="font-medium text-black dark:text-white">
-                                        {review.user_customer.fullname}
-                                    </h5>
-                                </td>
-                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                    <h5 className="font-medium text-black dark:text-white">
-                                        {review.products.name_product}
-                                    </h5>
-                                </td>
-                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                    <h5 className="font-medium text-black dark:text-white">
-                                        {review.order_list.code_order}
-                                    </h5>
-                                </td>
-                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                    <div className="flex items-center justify-center space-x-3.5">
-                                        <DetailReview id={review.id} /> 
-                                        <EditReview
-                                            review={review}
-                                            users={users}
-                                            products={products}
-                                            order_list={order_list}
-                                        />
-                                        <DeleteReview {...review} />
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const reviewsResponse = await axios.get("http://localhost:8000/api/review-products", {
+          withCredentials: true,
+        });
+        console.log("Reviews Data:", reviewsResponse.data);
+        setReviews(reviewsResponse.data);
+
+        const usersResponse = await axios.get("http://localhost:8000/api/user", {
+          withCredentials: true,
+        });
+        console.log("Users Data:", usersResponse.data);
+        setUsers(usersResponse.data);
+
+        const productsResponse = await axios.get("http://localhost:8000/api/products", {
+          withCredentials: true,
+        });
+        console.log("Products Data:", productsResponse.data);
+        setProducts(productsResponse.data);
+
+        const orderListResponse = await axios.get("http://localhost:8000/api/order", {
+          withCredentials: true,
+        });
+        console.log("Order List Data:", orderListResponse.data);
+        setOrderList(orderListResponse.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures useEffect runs once when the component mounts
+
+  let count = 1;
+
+  // Continue with the rest of your component logic, using the state variables
+
+  return (
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="py-6 px-4 md:px-6 xl:px-7.5">{/* <AddReview users={users} products={products} order_list={order_list} /> */}</div>
+
+      <div className="py-10 px-10">
+        <table className="table w-full">
+          <thead>
+            <tr className="bg-gray-2 text-left dark:bg-meta-4">
+              <th className="py-4 px-4 font-medium text-black dark:text-white">#</th>
+              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">Customer Name</th>
+              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">Product Name</th>
+              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">Code Order</th>
+              <th className="py-4 px-4 font-medium text-black dark:text-white">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reviews.data?.map((review, key) => (
+              <tr key={key}>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <h6 className="font-medium text-black dark:text-white">{review?.id}</h6>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <h5 className="font-medium text-black dark:text-white">{review?.users_customer?.fullname}</h5>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <h5 className="font-medium text-black dark:text-white">{review?.products?.name_product}</h5>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <h5 className="font-medium text-black dark:text-white">{review?.order_list?.code_order}</h5>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <div className="flex items-center justify-center space-x-3.5">
+                    <DetailReview id={review?.id} />
+                    <EditReview review={review} users={users} products={products} order_list={orderList} />
+                    <DeleteReview {...review} />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default TableReviewProducts;
