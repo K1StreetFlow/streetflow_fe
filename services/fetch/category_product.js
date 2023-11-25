2e7d15d1367f9129e62c140b7d5b41ba6955ba4c
rizkirifandi7 <rizki.10121284@mahasiswa.ac.id>
2023-11-26 01:15:01 +0700

    update order-list status dashboard

--- services/fetch/category_product.js
+++ services/fetch/category_product.js
@@ -1,57 +1,57 @@
-// import { instance } from "../axios/api";
+import { instance } from "../axios/api";
 
-// export const getAllCategory = async () => {
-// 	try {
-// 		const response = await instance.get("/category_products");
-// 		return response.data;
-// 	} catch (error) {
-// 		throw new Error(error.response.data.message || "Something went wrong");
-// 	}
-// };
+export const getAllCategory = async () => {
+	try {
+		const response = await instance.get("/category_products");
+		return response.data;
+	} catch (error) {
+		throw new Error(error.response.data.message || "Something went wrong");
+	}
+};
 
-// export const getPagination = async () => {
-// 	try {
-// 		const response = await instance.get("/category_products/pagination");
-// 		return response.data;
-// 	} catch (error) {
-// 		throw new Error(error.response.data.message || "Something went wrong");
-// 	}
-// };
+export const getPagination = async () => {
+	try {
+		const response = await instance.get("/category_products/pagination");
+		return response.data;
+	} catch (error) {
+		throw new Error(error.response.data.message || "Something went wrong");
+	}
+};
 
-// export const getCategory = async (id) => {
-// 	try {
-// 		const response = await instance.get(`/category_products/${id}`);
-// 		return response.data;
-// 	} catch (error) {
-// 		throw new Error(error.response.data.message || "Something went wrong");
-// 	}
-// };
+export const getCategory = async (id) => {
+	try {
+		const response = await instance.get(`/category_products/${id}`);
+		return response.data;
+	} catch (error) {
+		throw new Error(error.response.data.message || "Something went wrong");
+	}
+};
 
-// export const createCategory = async (data) => {
-// 	try {
-// 		const response = await instance.post("/category_products", data, {
-// 			headers: { "Content-Type": "multipart/form-data" },
-// 		});
-// 		return response.data;
-// 	} catch (error) {
-// 		throw new Error(error.response.data.message || "Something went wrong");
-// 	}
-// };
+export const createCategory = async (data) => {
+	try {
+		const response = await instance.post("/category_products", data, {
+			headers: { "Content-Type": "multipart/form-data" },
+		});
+		return response.data;
+	} catch (error) {
+		throw new Error(error.response.data.message || "Something went wrong");
+	}
+};
 
-// export const updateCategory = async (id, data) => {
-// 	try {
-// 		const response = await instance.put(`/category_products/${id}`, data);
-// 		return response.data;
-// 	} catch (error) {
-// 		throw new Error(error.response.data.message || "Something went wrong");
-// 	}
-// };
+export const updateCategory = async (id, data) => {
+	try {
+		const response = await instance.put(`/category_products/${id}`, data);
+		return response.data;
+	} catch (error) {
+		throw new Error(error.response.data.message || "Something went wrong");
+	}
+};
 
-// export const deleteCategory = async (id) => {
-// 	try {
-// 		const response = await instance.delete(`/category_products/${id}`);
-// 		return response.data;
-// 	} catch (error) {
-// 		throw new Error(error.response.data.message || "Something went wrong");
-// 	}
-// };
+export const deleteCategory = async (id) => {
+	try {
+		const response = await instance.delete(`/category_products/${id}`);
+		return response.data;
+	} catch (error) {
+		throw new Error(error.response.data.message || "Something went wrong");
+	}
+};
