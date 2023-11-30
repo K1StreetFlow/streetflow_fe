5f1471096cbd674c4635bfae625f77d726fffd21
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2023-12-01 00:22:35 +0700

    menambahkan review list

--- app/(ecommerce)/review/page.jsx
+++ app/(ecommerce)/review/page.jsx
@@ -1,38 +1,64 @@
 import React from "react";
 import "../../satoshi.css";
 import "../../globals.css";
-import ReviewPages from "@/components/Reviews/Reviews.jsx";
+import ReviewPages from "@/components/Reviews/Reviews-list";
 import { cookies } from "next/headers";
 
 async function getUserOrder() {
-	const cookieStore = cookies();
-	const token = cookieStore.get("tokenCustomer");
+  const cookieStore = cookies();
+  const token = cookieStore.get("tokenCustomer");
 
-	const res = await fetch("http://localhost:8000/api/order/user/orderList", {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			accept: "application/json",
-			cookie: `tokenCustomer=${token.value}`,
-		},
+  const res = await fetch("http://localhost:8000/api/order/user/orderList", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+      cookie: `tokenCustomer=${token.value}`,
+    },
 
-		credentials: "include",
-	});
+    credentials: "include",
+  });
 
-	return res.json();
+  return res.json();
+}
+
+async function getUserReview() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("tokenCustomer");
+
+  const res = await fetch(
+    `http://localhost:8000/api/review-products/user/review`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+        cookie: `tokenCustomer=${token.value}`,
+      },
+
+      credentials: "include",
+    }
+  );
+
+  return res.json();
 }
 
 const Review = async () => {
-	const orderdata = await getUserOrder();
-	const cookieStore = cookies();
-	const token = cookieStore.get("tokenCustomer");
-	return (
-		<>
-			<div className="container mx-auto px-4">
-				<ReviewPages orderdata={orderdata} token={token.value} />
-			</div>
-		</>
-	);
+  const orderdata = await getUserOrder();
+  const cookieStore = cookies();
+  const token = cookieStore.get("tokenCustomer");
+  const reviewdata = await getUserReview();
+  return (
+    <>
+      <div className="container mx-auto px-4">
+        <ReviewPages
+          orderdata={orderdata}
+          token={token.value}
+          review={reviewdata}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Review;
